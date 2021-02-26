f60b1d611b7b957bd3a4771815f4cfe22123f386
Takuro Fukunaga <takuro@nii.ac.jp>
2021-02-26 16:35:56 +0900

    update graph

--- codes/graph.cpp
+++ codes/graph.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <iostream>
 #include <sstream>
+#include <algorithm>
 
 #include "main.h"
 #include "graph.h"
@@ -16,8 +17,16 @@ Graph::Graph(int n){
   this->num_nodes = n;
   this->weight = vector <vector <int>>(n, vector <int>(n,-1));
   this->flag = vector <vector <int>>(n, vector <int>(n,0));
-}
 
+  this->node_names = vector <int>(n);
+  this->node_pointers = vector <int>(n);
+
+  REP(i, n){
+    this->node_names[i] = i;
+    this->node_pointers[i] = i;
+  }
+
+}
 
 Graph::Graph(const char *filename){
 
@@ -39,16 +48,23 @@ Graph::Graph(const char *filename){
       stringstream s{line};
       int cnt = 0;
       while(getline(s, line, ' ')){
-	if(cnt == 2){
-	  N = stoi(line);
-	  this->num_nodes = N;
-	  this->weight = vector <vector <int>>(N, vector <int>(N,-1));
-	  this->flag = vector <vector <int>>(N, vector <int>(N,0));
-	}
-	else if(cnt == 3){
-	  M = stoi(line);
-	}
-	cnt++;
+	      if(cnt == 2){
+	        N = stoi(line);
+	        this->num_nodes = N;
+	        this->weight = vector <vector <int>>(N, vector <int>(N,-1));
+	        this->flag = vector <vector <int>>(N, vector <int>(N,0));
+          this->node_names = vector <int>(N);
+          this->node_pointers = vector <int>(N);
+
+          REP(i, N){
+            this->node_names[i] = i;
+            this->node_pointers[i] = i;
+          }
+	      }
+	      else if(cnt == 3){
+	        M = stoi(line);
+	      }
+	      cnt++;
       }
     }
     else{
@@ -61,7 +77,7 @@ Graph::Graph(const char *filename){
       this -> weight[a-1][b-1] = 1;
       this -> weight[b-1][a-1] = 1;
     }
-    
+
     num++;
   }
 
@@ -69,10 +85,13 @@ Graph::Graph(const char *filename){
 }
 
 void Graph::delete_node(int i){
+  for(int &u : this->node_pointers) if(u == this->node_names[i]) u = -1;
+
   this->weight.erase(this->weight.begin()+i);
   this->flag.erase(this->flag.begin()+i);
   this->num_nodes--;
-  
+  this->node_names.erase(this->node_names.begin()+i);
+
   REP(j, this->num_nodes){
     this->weight[j].erase(this->weight[j].begin()+i);
     this->flag[j].erase(this->flag[j].begin()+i);
@@ -81,9 +100,16 @@ void Graph::delete_node(int i){
   return;
 }
 
+void Graph::delete_nodes(vector <int> U){
+  sort(U.begin(), U.end(), greater<int>());
+  for (auto u : U) this->delete_node(u);
+  return;
+}
+
+
 void Graph::merge_nodes(int a, int b){
   if(b < a) SWAP(int, a,b);
-  
+
   REP(k, this->num_nodes){
     if(k == a || k == b) continue;
 
@@ -93,8 +119,16 @@ void Graph::merge_nodes(int a, int b){
     if(this->flag[b][k] == -1) this->forbid(a,k);
   }
 
-  this->delete_node(b);
-  
+  for(int &u : this->node_pointers) if(u == this->node_names[b]) u = this->node_names[a];
+  this->weight.erase(this->weight.begin()+b);
+  this->flag.erase(this->flag.begin()+b);
+  this->num_nodes--;
+  this->node_names.erase(this->node_names.begin()+b);
+
+  REP(j, this->num_nodes){
+    this->weight[j].erase(this->weight[j].begin()+b);
+    this->flag[j].erase(this->flag[j].begin()+b);
+  }
   return;
 }
 
@@ -111,9 +145,43 @@ void Graph::permanent(int a, int b){
 }
 
 
+void Graph::delete_edge(int a, int b){
+  this->weight[a][b] = -1;
+  this->weight[b][a] = -1;
+  return;
+}
+
+
+void Graph::add_edge(int a, int b){
+  this->weight[a][b] = 1;
+  this->weight[b][a] = 1;
+  return;
+}
+
+
+bool Graph::conflict_triple (vector <int>& triple) const{
+  FOR(u, 0, this->num_nodes) FOR(v, 0, this->num_nodes){
+    if (u == v || this->weight[u][v] <= 0 || this->flag[u][v] == -1) continue;
+
+    FOR(w, 0, this->num_nodes){
+      if(u == w || v == w) continue;
+
+      if(this->weight[u][w] > 0 && this->flag[u][w] >= 0 && (this->weight[v][w] <= 0 || this->flag[v][w] == -1)){
+	      triple.clear();
+	      triple.push_back(u);
+	      triple.push_back(v);
+	      triple.push_back(w);
+	      return true;
+      }
+    }
+  }
+
+  return false;
+}
+
 void Graph::show () const{
   int n = this->num_nodes;
-  
+
   FOR(i, 0, n){
     cout << i << ": ";
     FOR(j, 0, n){
@@ -123,26 +191,4 @@ void Graph::show () const{
   }
 
   return;
-}
-
-
-bool Graph::conflict_triple (vector <int>& triple) const{
-
-  FOR(u, 0, this->num_nodes) FOR(v, 0, this->num_nodes){
-    if (u == v || this->weight[u][v] <= 0 || this->flag[u][v] == -1) continue;
-
-    FOR(w, 0, this->num_nodes){
-      if(u == w || v == w) continue;
-      
-      if(this->weight[u][w] > 0 && this->flag[u][w] >= 0 && (this->weight[v][w] <= 0 || this->flag[v][w] == -1)){
-	triple.clear();
-	triple.push_back(u);
-	triple.push_back(v);
-	triple.push_back(w);
-	return true;
-      }
-    }
-  }
-
-  return false;
 }
--- codes/graph.h
+++ codes/graph.h
@@ -1,21 +1,31 @@
 class Graph{
  public:
   int num_nodes;
-  std::vector <std::vector <int>> weight; 
+  std::vector <std::vector <int>> weight;
   std::vector <std::vector <int>> flag; // 1 -> permanent, -1 -> forbidden, 0 -> not restricted
+  std::vector <int> node_names;
+  std::vector <int> node_pointers;
 
   Graph();  // constructor
   Graph(int n);  // constructor with n nodes
   Graph(const char *filename); // constructor from the input file
-  void delete_node(int v); //; delete node v
-  void merge_nodes(int a, int b); // merge nodes a and b
-  void forbid(int a, int b);
-  void permanent(int a, int b);
-  
-  // return true if a conflict triple exists, false otherwise
+
+  void delete_node(int i);  // i is the current index
+  void delete_nodes(std::vector <int> U);
+
+  void merge_nodes(int a, int b); // merge nodes a and b (current indices)
+
+  void forbid(int a, int b); // a,b: current indices
+  void permanent(int a, int b); // a,b: current indices
+  void delete_edge(int a, int b); // a,b: current indices
+  void add_edge(int a, int b); // a,b: current indicies
+
+
+  // return false if no conflict triple exists,
+  //        true and set triple to a conflict triple otherwise
   bool conflict_triple(std::vector <int>& triple) const;
-  
-  
+
+
   // print weights
   void show() const;
 };
